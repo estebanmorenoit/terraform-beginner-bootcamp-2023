1888d34fc0853dae15b8b8a6b1a5c6ef355e17ec
Esteban Moreno <morenoramirezesteban@gmail.com>
2023-10-08 06:57:40 +0000

    #46 Fix issue with provider and set up two different towns

--- outputs.tf
+++ outputs.tf
@@ -1,14 +1,14 @@
 output "bucket_name" {
   description = "Bucket name for our static website hosting"
-  value       = module.terrahouse_aws.bucket_name
+  value       = module.terrahome_hosting.bucket_name
 }
 
 output "s3_website_endpoint" {
   description = "S3 Static Website Hosting Endpoint"
-  value       = module.terrahouse_aws.s3_website_endpoint
+  value       = module.terrahome_hosting.s3_website_endpoint
 }
 
 output "cloudfront_url" {
   description = "The CloudFront Distribution Domain Name"
-  value       = module.terrahouse_aws.clodfront_url
+  value       = module.terrahome_hosting.domain_name
 }
--- variables.tf
+++ variables.tf
@@ -1,7 +1,3 @@
-variable "bucket_name" {
-  type = string
-}
-
 variable "teacherseat_user_uuid" {
   type = string
 }
@@ -14,19 +10,9 @@ variable "terratowns_endpoint" {
   type = string
 }
 
-variable "index_html_filepath" {
-  type = string
-}
-
-variable "error_html_filepath" {
-  type = string
-}
-
-variable "content_version" {
-  type = number
-}
-
-variable "assets_path" {
-  description = "Path to assets folder"
-  type        = string
+variable "terrahome" {
+  type = object({
+    public_path     = string
+    content_version = number
+  })
 }
